5b30902858aa6665a20afb488ccde4cf2b7cc42d
ziyaarslan34 <zyarslnn@gmail.com>
2021-12-31 23:28:15 +0300

    first commit

---
+++ chronoTime.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <cstdlib>
+#include <unistd.h>
+
+struct Clock {
+	int hour, minute, second;
+	friend std::ostream& operator<<(std::ostream& out, const Clock &cl) {
+		out<<"\n[ "<<cl.hour<<" "<<cl.minute<<" "<<cl.second<<" ]\n";
+		return out;
+	}
+};
+
+std::string parseTime(const std::string &buffer) {
+	size_t cnt{};
+	std::string time;
+
+	for(size_t i=0;i<buffer.size();i++) {
+		if(buffer[i] >= 48 && buffer[i] <= 57)
+			cnt++;
+
+		if(cnt == 2) {
+			i += 2;
+			for(size_t j=i;buffer[i++] != ' ' && j < buffer.size();j++) {
+				time.push_back(buffer[j]);
+			}
+		}
+	}
+
+	return time;
+}
+
+Clock getCurrent(const std::string &currentTime) {
+	std::vector<int>vec;
+
+	for(size_t i=0;i<currentTime.size();) {
+		std::string tmp;
+
+		for(size_t j=i;currentTime[i++] != ':' && j < currentTime.size();j++)
+			tmp.push_back(currentTime[j]);
+		//vec.push_back(std::atoi(static_cast<const char*>(tmp)));
+		vec.push_back(std::stoi(tmp));
+	}
+
+	Clock res;
+
+	res.hour = vec[0];
+	res.minute = vec[1];
+	res.second = vec[2];
+
+	return res;
+}
+
+int main() {
+	sleep(1);
+
+	auto date = std::chrono::system_clock::now();
+	std::time_t time = std::chrono::system_clock::to_time_t(date);
+
+	std::time_t ttm = std::time(0);
+	std::tm *now = std::localtime(&ttm);
+
+	std::string buffer{std::ctime(&time)};
+	std::string currentTime = parseTime(buffer);
+
+	Clock clock = getCurrent(currentTime);
+
+
+	std::cout<<buffer<<"\n";
+	std::cout<<currentTime<<"\n";
+	//std::cout<<clock<<"\n";
+
+	if(((now->tm_year+1900) == 2022 &&
+		(now->tm_mon+1) == 1 && now->tm_mday == 1) &&
+	(clock.hour == 0 && clock.minute == 0 && clock.second == 0)) {
+		std::cout<<"\n\n"<<buffer<<"\n\n";
+		std::cout<<"Mutlu yillar\n\n";
+
+		return 0;
+	}
+
+
+	while(1) {
+		main();
+	}
+}
