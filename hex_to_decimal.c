da3409932fe54e16a16c535d6c82f953c7b2ad6b
ziyaarslan34 <zyarslnn@gmail.com>
2022-01-18 23:00:35 +0300

    fix bug

--- hex_to_decimal.c
+++ hex_to_decimal.c
@@ -10,26 +10,22 @@ int hex_to_dec(const char *num) {
 
 	for(size_t i=0;i<size;i++) {
 		switch(num[i]) {
-			case 97:
-				array[idx] = 10;
-				break;
-			case 98:
-				array[idx] = 11;
-				break;
-			case 99:
-				array[idx] = 12;
-				break;
-			case 100:
-				array[idx] = 13;
-				break;
-			case 101:
-				array[idx] = 14;
-			case 102:
-				array[idx] = 15;
-				break;
-			case 103:
-				array[idx] = 16;
-				break;
+			case 97:  array[idx] = 10; break;
+			case 98:  array[idx] = 11; break;
+			case 99:  array[idx] = 12; break;
+			case 100: array[idx] = 13; break;
+			case 101: array[idx] = 14; break;
+			case 102: array[idx] = 15; break;
+			case 103: array[idx] = 16; break;
+
+			case 65:  array[idx] = 10; break;
+            case 66:  array[idx] = 11; break;
+            case 67:  array[idx] = 12; break;
+            case 68:  array[idx] = 13; break;
+            case 69:  array[idx] = 14; break;
+            case 70:  array[idx] = 15; break;
+            case 71:  array[idx] = 16; break;
+
 			default:
 				array[idx] = num[i] - '0';
 				break;
@@ -49,4 +45,6 @@ int hex_to_dec(const char *num) {
 int main() {
 	printf("%d\n", hex_to_dec("123"));
 	printf("%d\n", hex_to_dec("7a"));
+	printf("%d\n", hex_to_dec("7A"));
+	printf("%d\n", hex_to_dec("36F"));
 }
