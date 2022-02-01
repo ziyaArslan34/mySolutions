7698555727ef4fd57b70b6a903c6c726c3b26c7b
ziyaarslan34 <zyarslnn@gmail.com>
2022-02-01 20:29:00 +0300

    uodate

--- void_ptr_2b.c
+++ void_ptr_2b.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void f(const void *ptr, size_t row, size_t col, size_t type, void (*fptr)(const void*)) {
+void print_2b_array(const void *ptr, size_t row, size_t col, size_t type, void (*fptr)(const void*)) {
 	for(size_t i=0;i<row*col;i++)
 			fptr(&*((char*)ptr+i*type));
 	printf("\n");
@@ -13,6 +13,6 @@ int main() {
 	char str[2][5] = {{"ziya"}, {"arsl"}};
 	int arr[2][5] = {{1,2,3,4,5},{6,7,8,9,10}};
 
-	f(str, 2, 5, sizeof(char), p_char);
-	f(arr, 2, 5, sizeof(int), p_int);
+	print_2b_array(str, 2, 5, sizeof(char), p_char);
+	print_2b_array(arr, 2, 5, sizeof(int), p_int);
 }
