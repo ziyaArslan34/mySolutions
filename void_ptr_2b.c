603af970ae50f20f012702a82e9adc8624b820b2
ziyaarslan34 <zyarslnn@gmail.com>
2022-02-01 20:26:07 +0300

    fix bug

--- void_ptr_2b.c
+++ void_ptr_2b.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 void f(const void *ptr, size_t row, size_t col, size_t type, void (*fptr)(const void*)) {
-	for(size_t i=0;i<row*col;i++) {
-			int ch = *((char*)ptr+i*type);
-			fptr(&ch);
-	}
-
+	for(size_t i=0;i<row*col;i++)
+			fptr(&*((char*)ptr+i*type));
 	printf("\n");
 }
 
@@ -14,10 +11,8 @@ void p_char(const void *val) { printf(" [%c] ", *(char*)val); }
 
 int main() {
 	char str[2][5] = {{"ziya"}, {"arsl"}};
-
 	int arr[2][5] = {{1,2,3,4,5},{6,7,8,9,10}};
 
 	f(str, 2, 5, sizeof(char), p_char);
-
 	f(arr, 2, 5, sizeof(int), p_int);
 }
