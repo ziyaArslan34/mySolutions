a61537da29ba78c61cd0f2ed114acf46206cc9fd
ziyaarslan34 <zyarslnn@gmail.com>
2022-02-01 20:22:54 +0300

    update

--- void_ptr_2b.c
+++ void_ptr_2b.c
@@ -13,11 +13,11 @@ void p_int(const void *val) { printf(" [%d] ", *(int*)val); }
 void p_char(const void *val) { printf(" [%c] ", *(char*)val); }
 
 int main() {
-	char str[2][4] = {{'z','i','y','a'}, {'a','r','s','l'}};
+	char str[2][5] = {{"ziya"}, {"arsl"}};
 
-	int arr[2][4] = {{1,2,3,4},{6,7,8,9}};
+	int arr[2][5] = {{1,2,3,4,5},{6,7,8,9,10}};
 
-	f(str, 2, 4, sizeof(char), p_char);
+	f(str, 2, 5, sizeof(char), p_char);
 
-	f(arr, 2, 4, sizeof(int), p_int);
+	f(arr, 2, 5, sizeof(int), p_int);
 }
