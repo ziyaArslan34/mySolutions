aa74f7b5931b9fcbab90ad081af72c0b74b26d72
ziyaarslan34 <zyarslnn@gmail.com>
2022-02-06 22:06:23 +0300

    uodate

--- optikTable.c
+++ optikTable.c
@@ -7,96 +7,113 @@
 #define ROW 10
 #define COL 10
 
-void printTable(char code[][COL], int *arr) {
-        printf("\n   ");
+void swap(void *val1, void *val2, size_t typeSize) {
+	void *temp = malloc(typeSize);
+	memcpy(temp, val1, typeSize);
+	memcpy(val1, val2, typeSize);
+	memcpy(val2, temp, typeSize);
+	free(temp);
+}
 
-        for(int i=0;i<ROW;i++) printf("\e[90m%d ", arr[i]);
-        printf("\n  ");
-        for(int i=0;i<20;i++) printf("\e[90m%c", '_');
-        printf("\n");
+int my_rand(int min, int max) {
+	return (int)rand()%(max-min+1)+min;
+}
 
-        for(int i=0;i<ROW;i++) {
-                printf("\e[90m%d |", i);
+void randomize_array(void *array, size_t size, size_t typeSize) {
+	for(size_t i=0;i<size;i++)
+		swap((char*)array+my_rand(0,(int)size-1)*typeSize, (char*)array+my_rand(0,(int)size-1)*typeSize, typeSize);
+}
 
-                for(int j=0;j<COL;j++) {
-                        if(code[i][j] <= 122 && code[i][j] >= 97) {
-                                printf("\e[91m%c\e[90m|", code[i][j]);
-                        } else {
-                                printf("\e[90m%c\e[90m|", code[i][j]);
-                        }
-                }
+void print_table(char code[][COL], int *arr) {
+	printf("\n   ");
 
-                printf("\n");
-        }
+	for(int i=0;i<ROW;i++)
+		printf("\e[90m%d ", arr[i]);
+	printf("\n  ");
 
-        char ch = '\xF8'; /*'¯'*/
+	for(int i=0;i<20;i++)
+		printf("\e[90m%c", '_');
+	printf("\n");
 
-        printf("  ");
-
-        for(int i=0;i<20;i++) printf("\e[90m%c", ch);
-        printf("\n");
+	for(int i=0;i<ROW;i++) {
+		printf("\e[90m%d |", i);
+		for(int j=0;j<COL;j++) {
+			if(code[i][j] <= 122 && code[i][j] >= 97) {
+				printf("\e[91m%c\e[90m|", code[i][j]);
+			} else {
+				printf("\e[90m%c\e[90m|", code[i][j]);
+			}
+		}
+		printf("\n");
+	}
+	char ch = '\xF8'; /*'¯'*/
+	printf("  ");
+	for(int i=0;i<20;i++)
+		printf("\e[90m%c", ch);
+	printf("\n");
 }
 
 int sep(int *arr, long int num) {
-        int ln, i=9;
+	int ln, i=9;
+	if(/*num < 100000000 ||*/ num > 9999999999)
+		return 0;
 
-        if(/*num < 100000000 ||*/ num > 9999999999) {
-                return 0;
-        }
-
-        while(num > 0) {
-                ln = num%10;
-                num /= 10;
-                arr[i--] = ln;
-        }
-        return 1;
+	while(num > 0) {
+		ln = num%10;
+		num /= 10;
+		arr[i--] = ln;
+	}
+	return 1;
 }
 
-void starMap(char code[][COL], int *arr) {
-        for(int i=0;i<ROW;i++) {
-                for(int j=0;j<COL;j++) {
-                        if(arr[j] == i) {
-                                code[i][j] = (char)rand()%(122-97)+97;
-                        }
-                }
-        }
+void star_map(char code[][COL], int *arr, const char *text) {
+	size_t idx=0;
+
+	for(int i=0;i<ROW;i++) {
+		for(int j=0;j<COL;j++) {
+			if(arr[j] == i) {
+				if(idx >= strlen(text))
+					idx=0;
+				code[i][j] = text[idx++];
+			}
+		}
+	}
 }
 
 int main() {
-        srand(time(NULL));
-        setlocale(LC_ALL, "utf-8");
+	srand((unsigned)time(NULL));
 
-        //while(1) {
-                system("clear");
-                int arr[10]={0};
+	char text[50];
+	printf("enter text: ");
+	fgets(text, sizeof(text), stdin);
+	text[strlen(text)-1] = '\0';
 
-                char code[ROW][COL] = {
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-                };
+	while(1) {
+		system("clear");
+		int arr[10]={0};
 
-                long int num;
+		char code[ROW][COL] = {
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+		};
 
-                printf("\nSayi gir: ");
-                scanf("%ld", &num);  // sayilari manuel girmek isterseniz
+		long int num = 1234567891;
 
-        //      num = (long int)rand()%(999999999-100000000)+100000000;
-
-                if(sep(arr, num)) {
-                        starMap(code, arr);
-                        printTable(code, arr);
-                } else {
-                        printf("\nLutfen 10 basamaktan fazla sayi girmeyin!\n");
-                }
-        //}
-
-        return 0;
+		if(sep(arr, num)) {
+			randomize_array(arr, 10, sizeof(int));
+			star_map(code, arr,text);
+			print_table(code, arr);
+		} else {
+			printf("\nLutfen 10 basamaktan fazla sayi girmeyin!\n");
+		}
+		sleep(1);
+	}
 }
