0f30ae1882469d1eae56c63a81819c4b758d590c
ziyaarslan34 <zyarslnn@gmail.com>
2022-02-18 23:46:37 +0300

    fixbug

--- fonksiyonel_makro.c
+++ fonksiyonel_makro.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 #define make_function(T) void print_##T(T val) {\
-	printf("[ ");\
 	if(strcmp(#T, "int") == 0) {\
+		printf("[ %d ] -> [ ", val);\
 		for(int i=(int)sizeof(val)*8-1;i>=0;i--)\
 			putchar(((val & 1 << i) ? '1' : '0'));\
 	} else if(strcmp(#T, "short") == 0) {\
+		printf("[ %hi ] -> [ ", val);\
 		for(int i=(int)sizeof(val)*8-1;i>=0;i--)\
 			putchar(((val & 1 << i) ? '1' : '0'));\
 	} else if(strcmp(#T, "char") == 0) {\
+		printf("[ %c ] -> [ ", val);\
 		for(int i=(int)sizeof(val)*8-1;i>=0;i--)\
 			putchar(((val & 1 << i) ? '1' : '0'));\
 	}\
@@ -25,6 +27,6 @@ int main() {
 	char z=97;
 
 	print_int(x);
-	print_char(z);
 	print_short(y);
+	print_char(z);
 }
