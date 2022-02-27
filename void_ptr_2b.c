849d9e312a9b2dbf2a7919b535b987bec60fa075
ziyaarslan34 <zyarslnn@gmail.com>
2022-02-27 23:41:11 +0300

    update

--- void_ptr_2b.c
+++ void_ptr_2b.c
@@ -12,7 +12,7 @@ struct test { int x,y; };
 void p_int(const void *val) { printf(" [%d] ", *(int*)val); }
 void p_char(const void *val) { printf(" [%c] ", *(char*)val); }
 void p_str(const void *val) { printf(" [%s] ", (char*)val); }
-void p_user(const void *val) { printf(" [%d] [%d] ",((struct test*)val)->x, ((struct test*)val)->y); }
+void p_user(const void *val) { printf(" [%d]-[%d] ",((struct test*)val)->x, ((struct test*)val)->y); }
 
 int main() {
 	char s[2][3][5] = {
@@ -27,4 +27,12 @@ int main() {
 	};
 
 	print_2b_array(t,2,2,sizeof(struct test), p_user);
+
+	int arr[3][3] = {
+		{1,2,3},
+		{4,5,6},
+		{7,8,9}
+	};
+
+	print_2b_array(arr, 3,3, sizeof(int), p_int);
 }
