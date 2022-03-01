509b17e3d9ab726b6010b688d3f5ab87bd0bffbf
ziyaarslan34 <zyarslnn@gmail.com>
2022-03-02 00:24:21 +0300

    uodate

--- Login/main.py
+++ Login/main.py
@@ -1,6 +1,6 @@
 import ctypes
 # gcc -fPIC -shared -o test.so main.c
 
-start = ctypes.CDLL("/data/data/com.termux/files/home/mySolutions/Login/src.so")
+start = ctypes.CDLL("/data/data/com.termux/files/home/mySolutions/Login/library.so")
 
 start.main()
