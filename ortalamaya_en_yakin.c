38a022cc18717d7e4d56609262c04bb4a4a1c216
ziyaarslan34 <zyarslnn@gmail.com>
2022-03-15 22:47:24 +0300

    fix bug

--- ortalamaya_en_yakin.c
+++ ortalamaya_en_yakin.c
@@ -9,6 +9,26 @@ int my_rand(int min, int max) {
 	return (int)rand()%(max-min+1)+min;
 }
 
+int **get_matris(size_t n) {
+	int **matris = NULL;
+
+	if((matris = (int**)malloc(sizeof(int*)*n)) == NULL) {
+		perror("memory error!! ");
+		exit(1);
+	}
+
+	for(size_t i=0;i<n;i++) {
+		if((matris[i] = (int*)malloc(sizeof(int)*n)) == NULL) {
+			perror("memory error!! ");
+			exit(1);
+		}
+		for(size_t j=0;j<n;j++)
+			matris[i][j] = my_rand(10,99);
+	}
+
+	return matris;
+}
+
 double get_average(int *array, size_t size) {
 	double average = 0.0;
 
@@ -17,35 +37,37 @@ double get_average(int *array, size_t size) {
 	return average/(double)size;
 }
 
-double get_max_average(int **array, size_t size) {
+double get_max_average(int **matris, size_t size) {
 	double average = 0.0;
 	for(size_t i=0;i<size;i++)
 		for(size_t j=0;j<size;j++)
-			average += array[i][j];
+			average += matris[i][j];
 
 	return average/(double)(size*size);
 }
 
 double get_nearest_average(double *averages, double maxAverage, size_t size) {
-	double *difference = NULL;
+	double *differences = NULL;
 	size_t idx = 0;
 
-	if((difference = (double*)malloc(sizeof(double)*size)) == NULL)
+	if((differences = (double*)malloc(sizeof(double)*size)) == NULL)
 		perror("memory error");
 
 	for(size_t i=0;i<size;i++)
-		difference[idx++] = GET_MAX_ELEMENTS(maxAverage, averages[i]) - GET_MIN_ELEMENTS(maxAverage, averages[i]);
+		differences[idx++] = GET_MAX_ELEMENTS(maxAverage, averages[i]) - GET_MIN_ELEMENTS(maxAverage, averages[i]);
 
 	double min = maxAverage;
 
 	for(size_t i=0;i<size;i++)
-		if(min > difference[i])
-			min = difference[i];
+		if(min > differences[i])
+			min = differences[i];
 
-	free(difference);
 	for(size_t i=0;i<size;i++)
-		if(min == difference[i])
+		if(min == differences[i])
 			return averages[i];
+
+	free(differences);
+
 	return 0.0;
 }
 
@@ -59,34 +81,27 @@ void print_matris(int **array, size_t size) {
 
 	printf("\n\n");
 }
+
 int main() {
 	srand((unsigned)time(NULL));
 
-	int **array = NULL;
+	int **matris = NULL;
 	size_t n=0;
 
 	printf("boyut girin: ");
 	scanf("%zu", &n);
 
-	if((array = (int**)malloc(sizeof(int*)*n)) == NULL)
-		perror("memory error\n");
-	for(size_t i=0;i<n;i++)
-		if((array[i] = (int*)malloc(sizeof(int)*n)) == NULL)
-			perror("memory error\n");
+	matris = get_matris(n);
+	print_matris(matris, n);
 
-	for(size_t i=0;i<n;i++)
-		for(size_t j=0;j<n;j++)
-			array[i][j] = my_rand(10,99);
-
-	print_matris(array, n);
 	double *averages = NULL;
-	double maxAverage = get_max_average(array, n);
+	double maxAverage = get_max_average(matris, n);
 
 	if((averages = (double*)malloc(sizeof(double)*n)) == NULL)
 		perror("memory error\n");
 
 	for(size_t i=0;i<n;i++)
-		averages[i] = get_average(array[i], n);
+		averages[i] = get_average(matris[i], n);
 
 	for(size_t i=0;i<n;i++)
 		printf("row %zu averages: %.3lf\n", i,averages[i]);
@@ -96,6 +111,6 @@ int main() {
 
 	free(averages);
 	for(size_t i=0;i<n;i++)
-		free(array[i]);
-	free(array);
+		free(matris[i]);
+	free(matris);
 }
