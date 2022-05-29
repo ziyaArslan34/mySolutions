cec2e243877ecbc7c727fa20d329ddbdb2314353
ziyaarslan34 <zyarslnn@gmail.com>
2022-05-29 21:51:33 +0300

    upd

--- findLocation.c
+++ findLocation.c
@@ -7,7 +7,11 @@ struct pair { size_t begin, end; };
 struct pair *find_location_all(const char *s1, const char *s2, size_t *len) {
 	size_t size=0, cap=4;
 
-	struct pair *p = NULL;
+	struct pair *p = (struct pair*)malloc(sizeof(struct pair)*cap);
+	if(!p) {
+		fprintf(stderr, "memory errror\n");
+		return NULL;
+	}
 
 	size_t lenSrc = strlen(s2);
 	size_t lenDest = strlen(s1);
@@ -19,12 +23,14 @@ struct pair *find_location_all(const char *s1, const char *s2, size_t *len) {
 			if(s1[m] == s2[j])
 				cnt++;
 		if(cnt == lenSrc) {
-			if(!p)
-				p = (struct pair*)malloc(sizeof(struct pair)*cap);
-
 			if(size >= cap) {
 				cap *= 2;
 				p = (struct pair*)realloc(p,sizeof(struct pair)*cap);
+				if(!p) {
+					fprintf(stderr, "memory error\n");
+					free(p);
+					return NULL;
+				}
 			}
 
 			p[size].begin = i;
@@ -39,16 +45,19 @@ struct pair *find_location_all(const char *s1, const char *s2, size_t *len) {
 
 int main() {
 	const char *dest = "mert mert mert mert mert";
-	const char *src = "shjx";
+	const char *src = "mert";
 
 	size_t size=0;
 
 	struct pair *p = find_location_all(dest, src, &size);
 
-	if(!p) {
-		printf("not found\n");
-		return -1;
+	if(!size) {
+		fprintf(stderr, "element not found\n");
+		return 1;
 	}
+
+	if(!p)
+		return 1;
 
 	for(size_t i=0;i<size;i++)
 		printf("\n[ %zu %zu ]\n", p[i].begin, p[i].end);
