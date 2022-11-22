e223e5ad76729790b75d278b29fc64eb5c7914d7
ziyaarslan34 <zyarslnn@gmail.com>
2022-11-22 15:03:00 +0300

    m

--- my_clock/main.c
+++ my_clock/main.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
+
 #include "clock.h"
 
-#define SIZE (10)
-
 int main(void) {
-	mytime_t system_clock = get_current_clock(get_current_time());
+	mytime_t current_time = get_current_clock(get_current_time());
+
 	printf("system clock: ");
-	print_clock(&system_clock);
-
-	mytime_t array[10];
-	for(size_t i=0;i<SIZE;i++)
-		array[i] = random_clock();
-
-	for(size_t i=0;i<SIZE;i++)
-		print_clock(array + i);
-
-	printf("\n--------After sort--------\n\n");
-	clock_sort(array, SIZE, comp_less);
-	for(size_t i=0;i<SIZE;i++)
-		print_clock(array + i);
+	print_clock(&current_time);
 }
