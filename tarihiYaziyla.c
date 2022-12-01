238a4a1ead556933f14ff260aea270f94869e407
ziyaarslan34 <zyarslnn@gmail.com>
2022-12-01 09:28:42 +0300

    mmm

--- tarihiYaziyla.c
+++ tarihiYaziyla.c
@@ -9,8 +9,8 @@
 
 #define SIZE 10
 
-void printDate(char date[][SIZE]) {
-	printf("\nDate Format: ");
+void print_date_text(char date[][SIZE]) {
+	printf("\ndate_text : ");
 	for(size_t i=0;i<SIZE;i++)
 		if(!(strcmp(date[i], " ") == 0))
 			printf("%s ", date[i]);
@@ -18,21 +18,21 @@ void printDate(char date[][SIZE]) {
 	printf("\n");
 }
 
-void init(char date[][SIZE]) {
+void init_array(char date[][SIZE]) {
 	for(size_t i=0;i<SIZE;i++) {
 		strcpy(date[i], "");
 	}
 }
 
-char **dateFormat(char *date) {
+void init_date_text(char *date) {
 	int len = strlen(date);
 
 	for(size_t i=0;i<len;i++)
-		if(!(date[i] >= 48 && date[i] <= 57) && !(date[i] == '.'))
-			return NULL;
+		if(!(date[i] >= '0' && date[i] <= '9') && !(date[i] == '.'))
+			return;
 
 	if((date[len-4] > '2' && date[len-3] > '0' && date[len-2] > '2') || (date[0] >= '3' && date[1] > '1') || date[2] != '.' || date[5] != '.' || date[3] > '1' || (date[3] == '1' && date[4] > '2') || len < 10 || len >= 11)
-		return NULL;
+		return;
 
 	char ones[SIZE][SIZE] = {
 		" ", "bir", "iki", "uc", "dort", "bes",
@@ -59,44 +59,44 @@ char **dateFormat(char *date) {
 		"temmuz", "agustos", "eylul", "ekim", "kasim", "aralik"
 	};
 
-	char returnDate[SIZE][SIZE];
-	init(returnDate);
+	char date_text[SIZE][SIZE];
+	init_array(date_text);
 
 	int cast;
 
 	cast = date[len-len] - '0';
-	strcpy(returnDate[len-len], tens[cast]);
+	strcpy(date_text[len-len], tens[cast]);
 
 	cast = date[len-len+1] - '0';
-	strcpy(returnDate[len-len+1], ones[cast]);
+	strcpy(date_text[len-len+1], ones[cast]);
 
 	if(date[len-len+3] == '1' && date[len-len+4] == '2') {
-		strcpy(returnDate[len-len+3], month[0]);
-		strcpy(returnDate[len-len+4], month[12]);
+		strcpy(date_text[len-len+3], month[0]);
+		strcpy(date_text[len-len+4], month[12]);
 	} else if(date[len-len+3] == '1' && date[len-len+4] == '1') {
-		strcpy(returnDate[len-len+3], month[0]);
-		strcpy(returnDate[len-len+4], month[11]);
+		strcpy(date_text[len-len+3], month[0]);
+		strcpy(date_text[len-len+4], month[11]);
 	} else {
 		cast = date[len-len+3] - '0';
-		strcpy(returnDate[len-len+3], month[cast]);
+		strcpy(date_text[len-len+3], month[cast]);
 
 		cast = date[len-len+4] - '0';
-		strcpy(returnDate[len-len+4], month[cast]);
+		strcpy(date_text[len-len+4], month[cast]);
 	}
 
 	cast = date[len-4] - '0';
-	strcpy(returnDate[len-4], thousand[cast]);
+	strcpy(date_text[len-4], thousand[cast]);
 
 	cast = date[len-3] - '0';
-	strcpy(returnDate[len-3], faces[cast]);
+	strcpy(date_text[len-3], faces[cast]);
 
 	cast = date[len-2] - '0';
-	strcpy(returnDate[len-2], tens[cast]);
+	strcpy(date_text[len-2], tens[cast]);
 
 	cast = date[len-1] - '0';
-	strcpy(returnDate[len-1], ones[cast]);
+	strcpy(date_text[len-1], ones[cast]);
 
-	return returnDate;
+	print_date_text(date_text);
 }
 
 int main() {
@@ -106,14 +106,5 @@ int main() {
 	scanf("%s", date);
 
 	printf("Tarih: %s\n", date);
-
-	if(dateFormat(date) != NULL) {
-		char **newDate = dateFormat(date);
-		printDate(newDate);
-	} else {
-		printf("\nLutfen tarihi gg.aa.yy seklinde ve olmayan gunleri, aylari, yillari girerek girmeyiniz\n");
-		return -1;
-	}
-
-	return 0;
+	init_date_text(date);
 }
